16363430283166ca2feeeebb116706ddeb8c89ed
Ehsanullah946 <Ehsanullahakbari53@gmail.com>
2024-07-02 09:37:14 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,29 @@
+powFlag=false;
+function addToCalculator(value){
+    document.getElementById('displayResult').value +=value;
+   if(powFlag){
+       var numbers= document.getElementById('displayResult').value.split('^');
+       document.getElementById('displayResult').value=Math.pow(numbers[0],numbers[1]);
+    
+     powFlag=false;
+   }
+}
+function finilize(){
+      document.getElementById('displayResult').value=eval( document.getElementById('displayResult').value);
+}
+function reset(){
+  var clear= document.getElementById('displayResult').value;
+   clear.innerHTML="";
+}
+
+
+function mathCalculator( mathFunc){
+    if(mathFunc=='pow'){
+        powFlag=true;
+        document.getElementById('displayResult').value+='^';
+    }else{
+        var val= document.getElementById('displayResult').value;
+        document.getElementById('displayResult').value =Math[mathFunc](val);
+    }
+ 
+}
